4888be53539fe919344c8cf48ec8936607140b6f
Matthew Wong <chunkitw@andrew.cmu.edu>
2025-06-05 00:46:29 +0800

    feat: introduce warehouse model

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 import os
 
 from modules.openai_client import AzureOpenAIClient, OpenAIClient
-from modules.csv_parser import load_categories_from_csv, parse_csv_to_input_data, parse_csv_to_demo_data
+from modules.csv_parser import load_categories_from_csv, load_warehouses_from_csv, parse_csv_to_input_data
 from modules.csv_writer import write_post_data_to_csv
 from modules.executor import process_batch_input_data
-from modules.sampler import Sampler
 
 # --- Configuration ---
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 CATEGORIES_FILE = os.path.join(CURRENT_DIR, "data/categories.csv")
+WAREHOUSES_FILE = os.path.join(CURRENT_DIR, "data/warehouses.csv")
 INPUT_DATA_FILE = os.path.join(CURRENT_DIR, "data/test.csv")
-DEMO_DATA_FILE = os.path.join(CURRENT_DIR, "data/demos.csv")
 OUTPUT_POST_DATA_FILE = os.path.join(CURRENT_DIR, "output.csv")
 
 NUM_CATEGORY_DEMOS = 5
@@ -24,13 +23,14 @@ def run_pipeline():
     try:
         print(f"Loading categories from: {CATEGORIES_FILE}")
         available_categories = load_categories_from_csv(CATEGORIES_FILE)
+
+        print(f"Loading warehouses from: {WAREHOUSES_FILE}")
+        warehouses = load_warehouses_from_csv(WAREHOUSES_FILE)
+        print(warehouses)
         
         print(f"Loading input data from: {INPUT_DATA_FILE}")
         input_items = parse_csv_to_input_data(INPUT_DATA_FILE)
 
-        print(f"Loading demo data from: {DEMO_DATA_FILE}")
-        all_demos = parse_csv_to_demo_data(DEMO_DATA_FILE) # Use your demo data parser
-        sampler_instance = Sampler(all_demo_data=all_demos) # Create ONE sampler instance
     except Exception as e:
         print(f"Failed to load initial data or initialize sampler: {e}")
         return
@@ -44,27 +44,27 @@ def run_pipeline():
 
     # 2. Initialize AI Client
     ai_client = OpenAIClient()
-    input_items = input_items[1:3]
+    input_items = input_items[0:2]
 
-    # 3. Process the batch of input data
-    print(f"\nProcessing {len(input_items)} items...")
-    generated_posts = process_batch_input_data(
-        input_data_list=input_items,
-        available_categories=available_categories,
-        ai_client=ai_client,
-        sampler=sampler_instance,
-        num_category_demos=NUM_CATEGORY_DEMOS
-    )
+    # # 3. Process the batch of input data
+    # print(f"\nProcessing {len(input_items)} items...")
+    # generated_posts = process_batch_input_data(
+    #     input_data_list=input_items,
+    #     available_categories=available_categories,
+    #     ai_client=ai_client,
+    #     sampler=sampler_instance,
+    #     num_category_demos=NUM_CATEGORY_DEMOS
+    # )
 
-    # 4. Write results to an output file
-    if generated_posts:
-        print(f"\nWriting {len(generated_posts)} generated posts to: {OUTPUT_POST_DATA_FILE}")
-        try:
-            write_post_data_to_csv(OUTPUT_POST_DATA_FILE, generated_posts)
-        except Exception as e:
-            print(f"Failed to write output data: {e}")
-    else:
-        print("No posts were generated.")
+    # # 4. Write results to an output file
+    # if generated_posts:
+    #     print(f"\nWriting {len(generated_posts)} generated posts to: {OUTPUT_POST_DATA_FILE}")
+    #     try:
+    #         write_post_data_to_csv(OUTPUT_POST_DATA_FILE, generated_posts)
+    #     except Exception as e:
+    #         print(f"Failed to write output data: {e}")
+    # else:
+    #     print("No posts were generated.")
 
     print("\n--- Post Generation Pipeline Finished ---")
 
